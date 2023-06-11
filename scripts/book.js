669f82f4f282fdea9dba54d1fb51442100304fdf
Jeremy Dufour <jeremy.k.dufour@gmail.com>
2023-06-11 17:36:31 -0400

    Progress 2

---
+++ scripts/book.js
@@ -0,0 +1,19 @@
+var curSlide;
+var selPetType;
+var selService;
+var selStaffMember;
+
+var backBtn;
+var nextBtn;
+
+var carousel;
+function onLoad(){
+    curSlide = 0;
+    backBtn = document.getElementById("prevBtn");
+    nextBtn = document.getElementById("prevBtn");
+    backBtn.style.display = "none";
+}
+
+function nextClick(){
+    console.log("")
+}
